a59536135ba1b61019a37c54c2d3d715f8ecccfb
dpolygon <dannyg@lonestardev.net>
2024-07-06 16:40:19 -0500

    Initial Commit

---
+++ pets/petsApp.swift
@@ -0,0 +1,17 @@
+//
+//  petsApp.swift
+//  pets
+//
+//  Created by Daniel Gonzalez on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct petsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
