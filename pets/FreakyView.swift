4e80f2c2adea5f00454d90b8b84541da35c00857
dpolygon <dannyg@lonestardev.net>
2024-07-09 15:33:38 -0500

    removing redundant code

--- pets/FreakyView.swift
+++ pets/FreakyView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct FreakyView: View {
+    @State private var rating: Int = 5
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(action: {
+                withAnimation {
+                    rating -= 1
+                }
+            }) {
+                Image(systemName: "minus.circle")
+            }
+            .disabled(rating == 0)
+            
+            Text(String(format: "%02d", rating))
+                .contentTransition(.numericText(value: Double(rating)))
+                .font(.largeTitle)
+                .fontDesign(.monospaced)
+                .frame(width: 45)
+                .fontWeight(.bold)
+            
+            Button(action: {
+                withAnimation {
+                    rating += 1
+                }
+            }) {
+                Image(systemName: "plus.circle")
+            }
+            .disabled(rating == 10)
+            
+        }
     }
 }
 
